5410f651f5fabf9cd5e5e5dd601ced2ac682e86d
blazejchwiecko <blazej@vormlab.com>
2015-10-23 23:01:12 +0200

    tabor added

---
+++ ParseStarterProject/ViewControllers/AddVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddVC.swift
+//  TravelMate
+//
+//  Created by Błażej Chwiećko on 23/10/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
