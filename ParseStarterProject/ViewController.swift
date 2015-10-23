e3391a6ff49a0678a704f924678ce3c0097d1539
Julian Engel <julianengel@users.noreply.github.com>
2015-10-23 21:47:36 +0200

    First commit with Parse

---
+++ ParseStarterProject/ViewController.swift
@@ -0,0 +1,34 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        let user = PFUser()
+        user.username = "my name"
+        user.password = "my pass"
+        user.email = "email@example.com"
+        
+        // other fields can be set if you want to save more information
+        user["phone"] = "650-555-0000"
+    
+            user.signUpInBackground()
+        
+                super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
